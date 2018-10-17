4fd643bb7add531cbc3fe5f7bb4e74182dfc1423
fja0kl <gaoexplicit@163.com>
2018-10-17 22:03:58 +0800

    递归

--- 437.Path-Sum-III/pathSum.cpp
+++ 437.Path-Sum-III/pathSum.cpp
@@ -6,17 +6,22 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
+ *
+ * findPath函数用来计算从当前点出发，路径和等于给定值；
+ * findPath完成树的遍历[先序遍历]
+ *
+ * pathSum 从每个点出发，计算等于给定值的路径和。
  */
 class Solution {
 public:
     int pathSum(TreeNode* root, int sum) {
         if(!root) return 0;
-        return helper(root,sum) + pathSum(root->left,sum) + pathSum(root->right,sum);
+        return findPath(root,sum) + pathSum(root->left,sum) + pathSum(root->right,sum);
     }
 private:
-    int helper(TreeNode* node, int sum){
+    int findPath(TreeNode* node, int sum){
         if(node == NULL)
             return 0;
-        return (node->val == sum ? 1 : 0) + helper(node->left,sum-node->val) + helper(node->right, sum-node->val);
+        return (node->val == sum ? 1 : 0) + findPath(node->left,sum-node->val) + findPath(node->right, sum-node->val);
     }
 };
