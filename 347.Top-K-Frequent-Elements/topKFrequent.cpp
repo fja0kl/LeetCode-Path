65a009b31f23ca2b5584f9fc9d117237dbfda1fb
fja0kl <gaoexplicit@163.com>
2018-10-24 20:15:30 +0800

    使用大顶堆完成排序

--- 347.Top-K-Frequent-Elements/topKFrequent.cpp
+++ 347.Top-K-Frequent-Elements/topKFrequent.cpp
@@ -1,19 +1,29 @@
-"""
-先计数：统计每个数字出现的次数；
-然后依据出现次数排序；
-取前k个项的键。
-"""
-class Solution:
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        cnt = {}
-        for i in nums:
-            cnt[i] = cnt.get(i, 0) + 1
-        b = sorted(cnt, key = (lambda key: cnt[key]), reverse=True)#直接对cnt字典排序，依据字典的值降序排序；然后保留字典的键值--b全是键值
-	# cnt.items():将cnt转换成了元组项，不可修改
+/**
+ * 思路和python解法相同：字典计数；排序
+ *
+ * 使用大顶堆完成对字典值进行排序
+ *
+ * 看看STL：好多方便的数据结构
+ *
+ */
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> cnt;
+        priority_queue<pair<int, int>> q;
+        vector<int> res;
         
-        return b[:k]
+        for(auto i : nums) ++cnt[i];
+        //大顶堆：priority_queue默认是大顶堆：
+        //添加后，自动构建大顶堆(默认按照键值排序--所以翻转顺序)
+        for (auto it : cnt) q.push({it.second, it.first});
+        
+        //前k个大顶推元素添加到res中
+        for(int i=0;i<k;i++){
+            res.push_back(q.top().second);
+            q.pop();
+        }
+        
+        return res;
+    }
+};
