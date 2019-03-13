88eff3b43cb44c182e10a1e8a6e94456527527ce
fja0kl <gaoexplicit@163.com>
2019-03-13 16:02:57 +0800

    模拟加法

--- CodingInterviews/不用加减乘除做加法/solution.cpp
+++ CodingInterviews/不用加减乘除做加法/solution.cpp
@@ -5,10 +5,10 @@ public:
         int sum, carray;
         //num1当做最终结果，num2为进位位--当进位位不为0时，循环
         while (num2 != 0){
-            sum = num1 ^ num2;
-            carray = (num1 & num2) << 1;
-            num1 = sum;
-            num2 = carray;
+            sum = num1 ^ num2;//不带进位计算加法
+            carray = (num1 & num2) << 1;//计算进位位
+            num1 = sum;//计算不带进位计算结果和进位相加的结果
+            num2 = carray;// 终止条件是进位位为0
         }
         return num1;
     }
